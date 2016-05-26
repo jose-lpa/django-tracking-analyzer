a5dba6afff3ccf8944c7d77a7627077e989af301
José L. Patiño <jose.lpa@gmail.com>
2016-05-26 18:38:55 +0100

    Added `StatisticsDashboard` view.

--- tracking_analyzer/admin.py
+++ tracking_analyzer/admin.py
@@ -1,9 +1,16 @@
+from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from .models import Tracker
 
 
+class StatisticsDashboard(TemplateView):
+    template_name = 'admin/stats_dashboard.html'
+
+
 class TrackerAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
     raw_id_fields = ['user']
     readonly_fields = [
         'content_type', 'object_id', 'ip_address', 'ip_country', 'ip_region',
@@ -18,12 +25,9 @@ class TrackerAdmin(admin.ModelAdmin):
         'user',
     ]
 
-    # Block up any admin user create/delete actions. Only programmers are
-    # intended to create new templates via data migrations.
+    # Block up any admin user create actions. `Tracker` instances are
+    # only data units to be seen or deleted.
     def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
         return False
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
@@ -38,6 +42,14 @@ class TrackerAdmin(admin.ModelAdmin):
 
         return super().change_view(
             request, object_id, form_url, extra_context=extra_context)
+
+    def get_urls(self):
+        urls = super(TrackerAdmin, self).get_urls()
+        my_urls = patterns(
+            '',
+            url(r'stats/$', self.admin_site.admin_view(StatisticsDashboard.as_view()), name='stats_dashboard'),
+        )
+        return my_urls + urls
 
 
 admin.site.register(Tracker, TrackerAdmin)
