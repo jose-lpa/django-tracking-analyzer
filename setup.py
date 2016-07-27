9bc3c588a3202c4e491b38003e06caed94abed3d
José L. Patiño <jose.lpa@gmail.com>
2016-07-27 01:41:36 +0100

    Bump version to 0.1b3.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django-tracking-analyzer',
-    version='0.1b2',
+    version='0.1b3',
     description='User actions tracking and analytics for Django sites.',
     author='José Luis Patiño Andrés',
     author_email='jose.lpa@gmail.com',
